d54b7bd28c0b7061c7f547558af91bd77d81cecc
Anubhav <kumaranubhav8844@gmail.com>
2021-12-08 20:33:49 +0500

    Update requests.js

--- scripts/requests.js
+++ scripts/requests.js
@@ -7,34 +7,3 @@ const getPromisePuzzle = async (wordCount) => {
         throw new Error('No puzzle found')
     }
 }
-
-const getCountry = async (countryCode) => {
-    const response =  await fetch(`https://restcountries.com/v3.1/all`,{})
-        if(response.status === 200) {
-            const data = await response.json()
-            country = data.find((country) => country.altSpellings[0] === countryCode)
-            return country.name.official
-            
-        } else {
-            throw new Error('No Country found')
-        }
-}
-
-
-const getLocation = async () =>{
-    const response = await fetch('https://ipinfo.io/json?token=bd4b56ec7393ce',{})
-        if(response.status === 200) {
-            const data = await response.json()
-            return data
-        } else {
-            throw new Error('Unable to get location')
-        }
-}
-
-
-const getCurrentCountry = async () => {
-    const data = await getLocation()
-    const country = await getCountry(data.country)
-    return country
-
-}
